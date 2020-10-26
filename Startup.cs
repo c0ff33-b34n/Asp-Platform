bbc6865478ecd9d69836bcb76c885b8c85a1fbc9
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-26 16:45:29 +0000

    Add Capital, Population routing and remove previously demonstrated middleware features

--- Startup.cs
+++ Startup.cs
@@ -13,84 +13,15 @@ namespace Platform
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<MessageOptions>(options =>
-            {
-                options.CityName= "Albany";
-            });
+        public void ConfigureServices(IServiceCollection services) {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
-            IOptions<MessageOptions> msgOptions)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Use(async (context, next) =>
-            {
-                if (context.Request.Path == "/location")
-                { 
-                    MessageOptions opts = msgOptions.Value;
-                    await context.Response.WriteAsync($"{opts.CityName}, {opts.CountryName}");
-                }
-                else
-                {
-                    await next();
-                }
-            });
-
-            app.Map("/branch", branch =>
-            {
-                branch.UseMiddleware<QueryStringMiddleware>();
-                
-                branch.Use(async (context, next) =>
-                {
-                    await context.Response.WriteAsync($"Branch Middleware");
-                });
-            });
-
-            app.Use(async (context, next) => 
-            {
-                await next();
-                await context.Response.WriteAsync($"\nStatus Code: { context.Response.StatusCode }");
-            });
-
-            app.Use(async (context, next) => 
-            {
-                if (context.Request.Path == "/short")
-                {
-                    await context.Response.WriteAsync($"Request Short Circuited");
-                }
-                else
-                {
-                    await next();
-                }
-            });
-
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseDeveloperExceptionPage();
+            app.UseMiddleware<Population>();
+            app.UseMiddleware<Capital>();
             app.Use(async (context, next) => {
-                if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
-                {
-                    await context.Response.WriteAsync("Custom Middleware \n");
-                }
-                await next();
-            });
-
-            app.UseMiddleware<QueryStringMiddleware>();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                await context.Response.WriteAsync("Terminal Middleware Reached");
             });
         }
     }
