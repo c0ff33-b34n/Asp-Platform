eb992abd7db74d87cfd8451093f551848ababb54
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-26 16:11:58 +0000

    Configure middleware with the options pattern

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace Platform
 {
@@ -16,15 +17,33 @@ namespace Platform
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<MessageOptions>(options =>
+            {
+                options.CityName= "Albany";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
+            IOptions<MessageOptions> msgOptions)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/location")
+                { 
+                    MessageOptions opts = msgOptions.Value;
+                    await context.Response.WriteAsync($"{opts.CityName}, {opts.CountryName}");
+                }
+                else
+                {
+                    await next();
+                }
+            });
 
             app.Map("/branch", branch =>
             {
