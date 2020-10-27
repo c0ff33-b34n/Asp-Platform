464fe0f96a997205fe207394b6cd9cddd6e8ce15
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-27 14:39:55 +0000

    Add service to WeatherEndpoint (which does not contain a constructor)

--- WeatherEndpoint.cs
+++ WeatherEndpoint.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Platform.Services;
 using System.Threading.Tasks;
 
 namespace Platform {
     public class WeatherEndpoint {
 
         public static async Task Endpoint(HttpContext context) {
-            await context.Response
-                .WriteAsync("Endpoint Class: It is cloudy in Milan");
+            IResponseFormatter formatter = context.RequestServices.GetRequiredService<IResponseFormatter>();
+            await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
         }
     }
 }
