35fdb8fc3a3d072d8079ff9cc02100248d7be083
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-27 13:56:11 +0000

    Add HtmlResponseFormatter and change IResponseFormatter type to demonstrate how much more easily it is to change implementation with type broker

--- Services/TypeBroker.cs
+++ Services/TypeBroker.cs
@@ -2,7 +2,7 @@ namespace Platform.Services
 {
     public class TypeBroker
     {
-        private static IResponseFormatter formatter = new TextResponseFormatter();
+        private static IResponseFormatter formatter = new HtmlResponseFormatter();
         public static IResponseFormatter Formatter => formatter;
     }
 }
