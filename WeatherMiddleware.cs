7a871c23513f9330f377987e706382b7b5ff203e
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-27 14:24:22 +0000

    Inject IResponseFormatter service into WeatherMiddleware

--- WeatherMiddleware.cs
+++ WeatherMiddleware.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Http;
+using Platform.Services;
 using System.Threading.Tasks;
 
 namespace Platform {
     public class WeatherMiddleware {
         private RequestDelegate next;
-
-        public WeatherMiddleware(RequestDelegate nextDelegate) {
+        private IResponseFormatter formatter;
+        
+        public WeatherMiddleware(RequestDelegate nextDelegate,IResponseFormatter respFormatter) 
+        {
             next = nextDelegate;
         }
 
         public async Task Invoke(HttpContext context) {
             if (context.Request.Path == "/middleware/class") {
-                await context.Response
-                    .WriteAsync("Middleware Class: It is raining in London");
+                await formatter.Format(context,
+                    "Middleware Class: It is raining in London");
             } else {
                 await next(context);
             }
