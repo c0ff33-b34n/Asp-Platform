d94bf81f44b4a8f619317632ff2a2c1c912f86b7
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-27 13:51:41 +0000

    Use type broker pattern to make implementation less tightly coupled

---
+++ Services/TypeBroker.cs
@@ -0,0 +1,8 @@
+namespace Platform.Services
+{
+    public class TypeBroker
+    {
+        private static IResponseFormatter formatter = new TextResponseFormatter();
+        public static IResponseFormatter Formatter => formatter;
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -25,7 +25,7 @@ namespace Platform
 
             app.Use(async (context, next) => {
                 if (context.Request.Path == "/middleware/function") {
-                    await TextResponseFormatter.Singleton.Format(context,
+                    await TypeBroker.Formatter.Format(context,
                         "Middleware Function: It is snowing in Chicago");
                 } else {
                     await next();
@@ -37,7 +37,7 @@ namespace Platform
                 endpoints.MapGet("/endpoint/class", WeatherEndpoint.Endpoint);
 
                 endpoints.MapGet("/endpoint/function", async context => {
-                    await TextResponseFormatter.Singleton.Format(context,
+                    await TypeBroker.Formatter.Format(context,
                         "Endpoint Function: It is sunny in LA");
                 });
             });
