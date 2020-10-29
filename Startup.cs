6e7bdaf5c2c156d8decdc4d8c0451c88311c90da
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-29 13:43:59 +0000

    Remove middleware and services, prepare for further demonstrations of features

--- Startup.cs
+++ Startup.cs
@@ -1,26 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Platform.Services;
 
 namespace Platform
 {
     public class Startup
     {
 
-        public Startup(IConfiguration config)
-        {
-            Configuration = config;
-        }
-        
-        private IConfiguration Configuration;
         public void ConfigureServices(IServiceCollection services) {
-            services.AddSingleton(typeof(ICollection<>), typeof(List<>));
+
         }
 
         public void Configure(IApplicationBuilder app)
@@ -30,28 +19,11 @@ namespace Platform
             
             app.UseRouting();
 
-            app.UseEndpoints(endpoints => {
-
-                endpoints.MapGet("/string", async context =>
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
                 {
-                    ICollection<string> collection
-                        = context.RequestServices.GetService<ICollection<string>>();
-                    collection.Add($"Request: { DateTime.Now.ToLongTimeString() }");
-                    foreach (string str in collection)
-                    {
-                        await context.Response.WriteAsync($"String: {str}\n");
-                    }
-                });
-                
-                endpoints.MapGet("/int", async context =>
-                {
-                    ICollection<int> collection
-                        = context.RequestServices.GetService<ICollection<int>>();
-                    collection.Add(collection.Count() + 1);
-                    foreach (int val in collection)
-                    {
-                        await context.Response.WriteAsync($"Int: {val}\n");
-                    }
+                    await context.Response.WriteAsync("Hello World!");
                 });
             });
         }
