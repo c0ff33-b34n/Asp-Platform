8d9d5e4bdb05e3906781e3d5a50a630fdc8aa9b6
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-29 11:30:43 +0000

    Create new handler for each request to remove need for WeatherEndpoint to know dependency can only be declared through endpoint method

--- Services/EndPointExtensions.cs
+++ Services/EndPointExtensions.cs
@@ -20,10 +20,14 @@ namespace Microsoft.AspNetCore.Builder
             T endpointInstance =ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
 
             ParameterInfo[] methodParams = methodInfo.GetParameters();
-            app.MapGet(path, context => (Task)methodInfo.Invoke(endpointInstance,
-                methodParams.Select(p => p.ParameterType == typeof(HttpContext)
-                ? context
-                : context.RequestServices.GetService(p.ParameterType)).ToArray()));
+            app.MapGet(path, context =>
+            {
+                T endpointInstance =ActivatorUtilities.CreateInstance<T>(context.RequestServices);
+
+                return (Task)methodInfo.Invoke(endpointInstance,
+                    methodParams.Select(p =>p.ParameterType == typeof(HttpContext) ? context :
+                        context.RequestServices.GetService(p.ParameterType)).ToArray());
+            });
         }
     }
 }
