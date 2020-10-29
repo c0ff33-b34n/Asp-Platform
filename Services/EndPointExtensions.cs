583cc2c912f743c8b862349b2ccf64ad279c83b5
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-29 10:59:19 +0000

    Create new transient IResponseFormatter for every Endpoint request in WeatherEndpoint

--- Services/EndPointExtensions.cs
+++ Services/EndPointExtensions.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +18,12 @@ namespace Microsoft.AspNetCore.Builder
                 throw new System.Exception("Method cannot be used");
             }
             T endpointInstance =ActivatorUtilities.CreateInstance<T>(app.ServiceProvider);
-            app.MapGet(path,(RequestDelegate)methodInfo
-                .CreateDelegate(typeof(RequestDelegate), endpointInstance));}
+
+            ParameterInfo[] methodParams = methodInfo.GetParameters();
+            app.MapGet(path, context => (Task)methodInfo.Invoke(endpointInstance,
+                methodParams.Select(p => p.ParameterType == typeof(HttpContext)
+                ? context
+                : app.ServiceProvider.GetService(p.ParameterType)).ToArray()));
+        }
     }
 }
--- WeatherEndpoint.cs
+++ WeatherEndpoint.cs
@@ -6,14 +6,7 @@ using System.Threading.Tasks;
 namespace Platform {
     public class WeatherEndpoint
     {
-        private IResponseFormatter formatter;
-        
-        public WeatherEndpoint(IResponseFormatter responseFormatter)
-        {
-            formatter = responseFormatter;
-        }
-
-        public async Task Endpoint(HttpContext context)
+        public async Task Endpoint(HttpContext context, IResponseFormatter formatter)
         {
             await formatter.Format(context, "Endpoint Class: It is cloudy in Milan");
         }
