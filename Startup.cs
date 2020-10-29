d97d16056d619ae358061919c4e8b95692feb9e7
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-29 16:18:04 +0000

    Set and delete cookies

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,8 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Logging;
-using static Platform.QueryStringMiddleware;
 
 namespace Platform
 {
@@ -23,8 +19,33 @@ namespace Platform
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapFallback(async context =>
-                    await context.Response.WriteAsync("Hello World!"));
+                endpoints.MapGet("/cookie", async context =>
+                {
+                    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
+                    context.Response.Cookies.Append("counter1", counter1.ToString(),
+                        new CookieOptions
+                        {
+                            MaxAge = TimeSpan.FromMinutes(30)
+                        }
+                    );
+                    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+                    context.Response.Cookies.Append("counter2", counter1.ToString(),
+                        new CookieOptions
+                        {
+                            MaxAge = TimeSpan.FromMinutes(30)
+                        }
+                    );
+                    await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+                });
+
+                endpoints.MapGet("clear", context =>
+                {
+                    context.Response.Cookies.Delete("counter1");
+                    context.Response.Cookies.Delete("counter2");
+                    context.Response.Redirect("/");
+                    return Task.CompletedTask;
+                });
+
             });
         }
     }
