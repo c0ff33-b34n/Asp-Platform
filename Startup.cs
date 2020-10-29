ff2e4f29ae4be4e3e7b041153b4ee57ede4104dc
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-29 14:15:52 +0000

    Access configuration settings (IConfiguration) from appsettings.json files

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Platform
@@ -12,12 +14,18 @@ namespace Platform
 
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IConfiguration config)
         {
             
             app.UseDeveloperExceptionPage();
             
             app.UseRouting();
+
+            app.Use(async (context, next) => 
+            {
+                string defaultDebug = config["Logging:LogLevel:Default"];
+                await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
+            });
 
             app.UseEndpoints(endpoints =>
             {
