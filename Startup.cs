ffa3744a00b6034de02d77e7364f43612fe31f07
Stuart Bowes <stuartbowes2000@gmail.com>
2020-10-30 11:12:43 +0000

    Recreate counter using session data

--- Startup.cs
+++ Startup.cs
@@ -36,31 +36,16 @@ namespace Platform
             {
                 endpoints.MapGet("/cookie", async context =>
                 {
-                    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter1", counter1.ToString(),
-                        new CookieOptions
-                        {
-                            MaxAge = TimeSpan.FromMinutes(30),
-                            IsEssential = true
-                        }
-                    );
-                    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter2", counter1.ToString(),
-                        new CookieOptions
-                        {
-                            MaxAge = TimeSpan.FromMinutes(30)
-                        }
-                    );
-                    await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
+                    context.Session.SetInt32("counter1", counter1);
+                    context.Session.SetInt32("counter2", counter2);
+                    await context.Session.CommitAsync();
+                    await context.Response.WriteAsync($"Conuter1: {counter1}, Counter2: {counter2}");
                 });
 
-                endpoints.MapGet("clear", context =>
-                {
-                    context.Response.Cookies.Delete("counter1");
-                    context.Response.Cookies.Delete("counter2");
-                    context.Response.Redirect("/");
-                    return Task.CompletedTask;
-                });
+                endpoints.MapFallback(async context =>
+                    await context.Response.WriteAsync("Hello World!"));
 
             });
         }
